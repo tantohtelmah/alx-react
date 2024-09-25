2c234c77c41f60956a3576b0a66db2570029fe94
tantohtelmah <tantohtelmah@gmail.com>
2024-09-25 19:58:03 -0400

    tasks

---
+++ 0x01-react_intro/task_4/dashboard/dashboard/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
